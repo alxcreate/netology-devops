88893738c41b618acbd41684bea45e6d9eb20928
Aleksei <alxaustralia@gmail.com>
2022-08-09 21:22:29 +0700

    add homework

---
+++ Homeworks/01-sysadm-homeworks/04-script-03-yaml/script.py
@@ -0,0 +1,25 @@
+import json
+import yaml
+import socket
+from time import sleep
+
+List = {
+    'drive.google.com': '',
+    'mail.google.com': '',
+    'google.com': ''
+}
+
+
+while True:
+    for Name in List:
+        ip = socket.gethostbyname(Name)
+        print(f'{Name} - {ip}')
+        with open(f'{Name}.json', 'w') as jsn:
+            jsn.write(json.dumps({Name: ip}))
+        with open(f'{Name}.yaml', 'w') as yml:
+            yml.write(yaml.dump([{Name: ip}]))
+        if ip != List[Name]:
+            print(f'[ERROR] {Name} IP mismatch: {List[Name]} {ip}')
+        List[Name] = ip
+    sleep(10)
+
