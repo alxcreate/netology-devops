0665a6ee8c384b2621ae53efeceda4ea16820bc8
Aleksei <alxaustralia@gmail.com>
2022-09-15 23:32:25 +0700

    upload homework

---
+++ Homeworks/02-virt-homeworks/05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g6f9ktskg20km27fo6"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gepbnbb1dpav5bstcs"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd80o438k5ni5e72j9ig"
+}
