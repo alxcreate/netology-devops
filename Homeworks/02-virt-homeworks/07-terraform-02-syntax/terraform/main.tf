f7e1cf885486a48c8639d01969ea1c129339b90b
Aleksei <alxaustralia@gmail.com>
2022-11-09 19:25:00 +0100

    Test

--- Homeworks/02-virt-homeworks/07-terraform-02-syntax/terraform/main.tf
+++ Homeworks/02-virt-homeworks/07-terraform-02-syntax/terraform/main.tf
@@ -1,12 +1,47 @@
-provider "yandex" {
-  token                    = "auth_token_here"
-  service_account_key_file = "path_to_service_account_key_file"
-  cloud_id                 = "cloud_id_here"
-  folder_id                = "folder_id_here"
-  zone                     = "ru-central1-a"
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
 }
 
-// Create a new instance
+provider "yandex" {
+  token     = $yc_token
+  cloud_id  = "b1g6f9ktskg20km27fo6"
+  folder_id = "b1gepbnbb1dpav5bstcs"
+  zone      = "ru-central1-a"
+}
+
 resource "yandex_compute_instance" "default" {
-  ...
+  name        = "test"
+  platform_id = "standard-v1"
+  zone        = "ru-central1-a"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "image_id"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.foo.id}"
+  }
+
+  metadata = {
+    foo      = "bar"
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "foo" {}
+
+resource "yandex_vpc_subnet" "foo" {
+  zone       = "ru-central1-a"
+  network_id = "${yandex_vpc_network.foo.id}"
 }
