12bf2ab3dcc19b1ec12984c354b561dfed1221fa
alxcreate <alxaustralia@gmail.com>
2023-01-17 22:44:20 +0100

    homework

---
+++ Homeworks/03-mnt-homeworks/08-ansible-03-yandex/playbook/templates/08-ansible-02-playbook/terraform/variables.tf
@@ -0,0 +1,25 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g6f9ktskg20km27fo6"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gepbnbb1dpav5bstcs"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8jvcoeij6u9se84dt5"
+}
+
+variable "ubuntu-2204-base" {
+  default = "fd8ueg1g3ifoelgdaqhb"
+}
+
+variable "centos-8-base" {
+  default = "fd8151sv1q69mchl804a"
+}
