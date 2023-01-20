52f9fa94f0aa301db333d55dc9d7521169a544cd
alxcreate <alxaustralia@gmail.com>
2023-01-20 21:26:47 +0100

    homework

--- Homeworks/03-mnt-homeworks/08-ansible-03-yandex/playbook/templates/08-ansible-02-playbook/terraform/output.tf
+++
@@ -1,7 +0,0 @@
-output "internal_ip_address_clickhouse-01_yandex_cloud" {
-  value = yandex_compute_instance.clickhouse-01.network_interface.0.ip_address
-}
-
-output "external_ip_address_clickhouse-01_yandex_cloud" {
-  value = yandex_compute_instance.clickhouse-01.network_interface.0.nat_ip_address
-}
