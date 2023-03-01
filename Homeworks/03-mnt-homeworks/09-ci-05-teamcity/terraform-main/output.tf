e298c813a64ac08941e10ab89977511ec5ab482e
alxcreate <alxaustralia@gmail.com>
2023-03-01 20:34:55 +0100

    homework

--- Homeworks/03-mnt-homeworks/09-ci-05-teamcity/terraform-main/output.tf
+++ Homeworks/03-mnt-homeworks/09-ci-05-teamcity/terraform-main/output.tf
@@ -13,3 +13,11 @@ output "internal_ip_address_teamcity-server_yandex_cloud" {
 output "external_ip_address_teamcity-server_yandex_cloud" {
   value = yandex_compute_instance.teamcity-server.network_interface.0.nat_ip_address
 }
+
+output "internal_ip_address_vm_yandex_cloud" {
+  value = yandex_compute_instance.vm.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_yandex_cloud" {
+  value = yandex_compute_instance.vm.network_interface.0.nat_ip_address
+}
---
+++ Homeworks/03-mnt-homeworks/09-ci-05-teamcity/terraform-main/vm.tf
@@ -0,0 +1,29 @@
+resource "yandex_compute_instance" "vm" {
+  name                      = "vm"
+  zone                      = "ru-central1-a"
+  hostname                  = "vm.netology.cloud"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+    metadata = {
+    ssh-keys = "vm:${file("~/.ssh/id_rsa.pub")}"
+
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8151sv1q69mchl804a"
+      name     = "root-vm"
+      type     = "network-nvme"
+      size     = "50"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.default.id
+    nat       = true
+  }
+}
