28a929029cf04bc6e8c3f305346fd7a5a3e44695
alxcreate <alxaustralia@gmail.com>
2023-03-01 20:39:17 +0100

    homework

--- Homeworks/03-mnt-homeworks/09-ci-05-teamcity/terraform-main/output.tf
+++ Homeworks/03-mnt-homeworks/09-ci-05-teamcity/terraform-main/output.tf
@@ -1,18 +1,18 @@
-output "internal_ip_address_teamcity-agent_yandex_cloud" {
-  value = yandex_compute_instance.teamcity-agent.network_interface.0.ip_address
-}
+#output "internal_ip_address_teamcity-agent_yandex_cloud" {
+#  value = yandex_compute_instance.teamcity-agent.network_interface.0.ip_address
+#}
 
-output "external_ip_address_teamcity-agent_yandex_cloud" {
-  value = yandex_compute_instance.teamcity-agent.network_interface.0.nat_ip_address
-}
+#output "external_ip_address_teamcity-agent_yandex_cloud" {
+#  value = yandex_compute_instance.teamcity-agent.network_interface.0.nat_ip_address
+#}
 
-output "internal_ip_address_teamcity-server_yandex_cloud" {
-  value = yandex_compute_instance.teamcity-server.network_interface.0.ip_address
-}
+#output "internal_ip_address_teamcity-server_yandex_cloud" {
+#  value = yandex_compute_instance.teamcity-server.network_interface.0.ip_address
+#}
 
-output "external_ip_address_teamcity-server_yandex_cloud" {
-  value = yandex_compute_instance.teamcity-server.network_interface.0.nat_ip_address
-}
+#output "external_ip_address_teamcity-server_yandex_cloud" {
+#  value = yandex_compute_instance.teamcity-server.network_interface.0.nat_ip_address
+#}
 
 output "internal_ip_address_vm_yandex_cloud" {
   value = yandex_compute_instance.vm.network_interface.0.ip_address
